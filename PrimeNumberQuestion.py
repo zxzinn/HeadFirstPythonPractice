666fe96f3233ea0b66acba13231ed2245159b894
ZhaoXinn <410580169@gapp.fju.edu.tw>
2021-10-25 04:27:51 +0800

    PrimeNumberQuestion 0.1

--- PrimeNumberQuestion.py
+++ PrimeNumberQuestion.py
@@ -1,21 +1,31 @@
 
 
-def FindPrimeNumber():
-    PrimeList = [] # 此空List拿來存所取範圍的所有質數
-    i = int(input()) # 要取的數
-    for i in range(2, i):
+#不使用def含式的原因是因為回傳的值的類型會是"NoneType"，此類型無法做任何處理 例如取長度len(),個數size()
 
-         for j in range(2, i):
-             if i % j == 0:
-                 break
-         #2到j若全部都無法被i整除，則此樹為質數，將i加入列表，並且尋找下一個質數i
+i = int(input())
+#取小於i的所有質數
+PrimeList = []
+# 此空List拿來存所取範圍的所有質數
+
+for i in range(1, i):
+
+     for j in range(2, i):
+         if i % j == 0:
+             break
+     #2到j若全部都無法被i整除，則此樹為質數，將i加入列表，並且尋找下一個質數i
 
 
-         else:
-             PrimeList.append(i)# 將質數加入質數列
+     else:
+         PrimeList.append(str(i))# 將質數加入質數列
 
-    print(PrimeList)
+print(PrimeList)
 
-FindPrimeNumber()
+
+print("在i內所有質數的數量為", len(PrimeList))
+# 若要取在i內所有質數的數量 只要將PrimeList的長度print出來
+
+
+
+
 
 
